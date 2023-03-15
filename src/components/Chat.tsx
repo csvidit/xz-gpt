5c454eca96922ae9bfde3c301631a08fd57f3a33
Vidit Khandelwal <viditk17@gmail.com>
2023-03-15 15:34:05 -0400

    opengraph and other optimizations

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -24,14 +24,14 @@ const Chat = (props: { user: UserProfile | undefined }) => {
   }) {
     setPrompt(event.target.value);
   }
-  function handleHumanizeChange(event: any){
-    setHumanize(!humanize)
-    console.log("Humanize Value "+humanize.valueOf());
+  function handleHumanizeChange(event: any) {
+    setHumanize(!humanize);
+    console.log("Humanize Value " + humanize.valueOf());
   }
 
   useEffect(() => {
-    console.log("Humanize Value "+humanize.valueOf());
-  }, [humanize])
+    console.log("Humanize Value " + humanize.valueOf());
+  }, [humanize]);
 
   const generate = async () => {
     console.log(prompt);
@@ -42,8 +42,7 @@ const Chat = (props: { user: UserProfile | undefined }) => {
       setResponse("Response in progress...");
       setIsLoading(true);
       let request = prompt;
-      if(humanize)
-      {
+      if (humanize) {
         request += ". I want you to humanize your response";
       }
       const completion = await openai.createChatCompletion({
@@ -75,7 +74,7 @@ const Chat = (props: { user: UserProfile | undefined }) => {
   return (
     <div className="mt-10 w-full lg:w-1/2 flex flex-col space-y-5">
       <textarea
-        className="prompt textarea font-sans p-2 lg:p-4 rounded-xl w-full bg-slate-900 bg-opacity-10 focus:border-2 focus:border-blue-500 placeholder-blue-500"
+        className="prompt textarea font-sans p-2 lg:p-4 rounded-xl w-full bg-slate-900 bg-opacity-10 focus:border-2 focus:border-blue-600 placeholder-blue-600"
         placeholder="Write your prompt here..."
         onChange={handlePromptChange}
         value={prompt}
@@ -96,10 +95,13 @@ const Chat = (props: { user: UserProfile | undefined }) => {
         {!isLoading && (
           <div className="form-control">
             <label className="cursor-pointer label flex-row space-x-1 items-center">
-              <span className="label-text text-neutral-900 text-base">humanize?</span>
+              <span className="label-text text-neutral-900 text-base">
+                humanize?
+              </span>
               <input
+                data-theme="dracula"
                 type="checkbox"
-                className="toggle bg-neutral-200 checked:bg-blue-500"
+                className="toggle toggle-primary bg-neutral-200 checked:bg-blue-500"
                 onChange={() => setHumanize(!humanize)}
                 checked={humanize}
               />
