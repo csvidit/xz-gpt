9aceb75ce37c18d158c49fb3ceeea6e2e79d33ef
Vidit Khandelwal <viditk17@gmail.com>
2023-03-21 12:53:13 -0400

    added abbreviation for history item labels

--- src/components/HistoryItem.tsx
+++ src/components/HistoryItem.tsx
@@ -1,11 +1,13 @@
 import Markdown from "markdown-to-jsx";
 
 const HistoryItem = (props: {key: any, id: string | null | undefined, label: string, children: any}) => {
+
+  const labelToDisplay: string = props.label.length > 100 ? props.label.substring(0, 100)+"..." : props.label;
   console.log(props.key);
   return (
     <div className="flex flex-col items-center font-sans">
-      <label htmlFor={"my-modal-"+props.id} className="btn text-neutral-900 font-normal capitalize p-4 bg-neutral-900 bg-opacity-10 hover:bg-opacity-30 border-0 rounded-full h-full w-full lg:w-1/2">
-        {props.label}
+      <label htmlFor={"my-modal-"+props.id} className="btn text-neutral-900 font-normal capitalize p-4 bg-neutral-900 bg-opacity-10 hover:bg-opacity-30 border-0 rounded-full h-14 w-full lg:w-1/2">
+        {labelToDisplay}
       </label>
       <input type="checkbox" id={"my-modal-"+props.id} className="modal-toggle" />
       <div className="modal text-neutral-200 backdrop-blur-md">
